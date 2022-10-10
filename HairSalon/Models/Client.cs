b3dcea8af4531f105229c03908c7ef296690c0a0
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-10-10 09:25:04 -0700

    updated models to accomidate from ToDoList

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,8 +3,10 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; }
+    public int Price { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
