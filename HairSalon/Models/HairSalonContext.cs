76052ce3fb1b8e595cd007c2a70c6dbda09ec78f
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-10-17 04:11:36 -0700

    added stylist controller and updated HairSalonContext

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,11 +4,10 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    public DbSet<Category> Stylists { get; set; }
-    public DbSet<Item> Clients { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
-    public HairSalonContext(DbContextOptions options) : base(options) { }
-
+    public HairSalonContext(DbContextOptions options) : base(options){}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseLazyLoadingProxies();
