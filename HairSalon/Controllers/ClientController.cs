440d5d019826ac895406bf4a38f3ac0b384f93de
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-10-17 04:57:11 -0700

    updated to add sql funciton bug testing website to function properly

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -7,39 +7,39 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
-  public class StylistController : Controller 
+  public class ClientController : Controller 
   { 
 
     private readonly HairSalonContext _db;
 
-    public StylistController(HairSalonContext db) 
+    public ClientController(HairSalonContext db) 
     {
       _db = db; 
     }
 
     public ActionResult Index()
     {
-      List<Client> model = _db.HairSalon.Include(stylist => stylist.Name).ToList(); 
+      List<Client> model = _db.Clients.Include(client => client.Name).ToList(); 
       return View(model);
     }
 
     public ActionResult Create()
     {
-      ViewBag.StylistId = new SelectList(_db.Types, "StylistId", "Name");
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View(); 
     }
 
     [HttpPost]
     public ActionResult Create(Client client)
     {
-      _db.Clients.Add(clients); 
+      _db.Clients.Add(client); 
       _db.SaveChanges(); 
       return RedirectToAction("Index"); 
     }
 
     public ActionResult Show(int id)
     {
-      Client thisClient = _db.Clients.FirstOrDefault(cliend => client.ClientId == id); 
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id); 
       return View(thisClient);
     }
 
